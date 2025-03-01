6c60c65a37e63100539e59117b01f218a6f6564d
senseix21 <mahdix2118@protonmail.com>
2025-03-01 12:21:07 +0600

    Initialize main server setup with route registration

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,10 +1,26 @@
 package main
 
-import "sensei/internal"
+import (
+	"net"
+	"sensei/internal"
+)
 
 func main() {
 	//Create new server
 	server := internal.NewServer(":8080")
+
+	// Register routes
+	server.Router.Handle("GET", "/", func(conn net.Conn, path string) {
+		response := "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n\r\nWelcome to Sensei!"
+		conn.Write([]byte(response))
+	})
+
+	server.Router.Handle("GET", "/hello", func(conn net.Conn, path string) {
+		response := "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n\r\nHello, World!"
+		conn.Write([]byte(response))
+	})
+
+	server.Start()
 
 	//Start the server
 	server.Start()
